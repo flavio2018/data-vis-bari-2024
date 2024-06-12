2cc3a462fd4cc8969ef18aaa887bded3e2cf132e
Flavio <flavio.petruzzellis@gmail.com>
2024-06-12 15:22:12 +0200

    new tab

--- src/web.py
+++ src/web.py
@@ -3,6 +3,8 @@ import plotly
 import pandas as pd 
 import plotly.figure_factory as ff
 import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
@@ -24,7 +26,7 @@ def get_sezioni_to_plesso(df_sedi_seggio):
 
 def plot_hexmap_party(df, party_name):
 	fig = ff.create_hexbin_mapbox(
-	    data_frame=filtered_df_voti, 
+	    data_frame=df, 
 	    #labels={"hover_name": 'PLESSO'},
 	    lat="lat", lon="lng",
 	    nx_hexagon=40, opacity=0.7, 
@@ -43,6 +45,12 @@ def plot_hexmap_party(df, party_name):
 	st.plotly_chart(fig, theme=None)
 
 
+def reg_plot_parties(df_voti_perc, party1, party2):
+	fig, ax = plt.subplots(1, 1, figsize=(7, 6))
+	ax = sns.regplot(data=df_voti_perc, x=party1, y=party2, ax=ax)
+	st.pyplot(fig)
+    
+
 # px.set_mapbox_access_token(open("../.mapbox_token").read())
 
 df_sedi_seggio = pd.read_csv('./data/sedi_seggio.csv')
@@ -53,8 +61,6 @@ sezioni_to_plesso = get_sezioni_to_plesso(df_sedi_seggio)
 df_voti_liste_comunali_sezioni = add_plesso_to_df(df_voti_liste_comunali_sezioni, sezioni_to_plesso)
 
 df_voti_joined = df_voti_liste_comunali_sezioni.join(df_sedi_seggio_geocoded.set_index('numero_plesso'), 'PLESSO')
-
-filtered_df_voti = df_voti_joined[df_voti_joined['formatted_address'].str.count('Bari BA, Italy') > 0]
 
 partiti_o_coalizioni = ['SCIACOVELLI SINDACO - CI PIACE!',
        'NOI PER BARI - ITALEXIT PER L\'ITALIA PER SCIACOVELLI SINDACO',
@@ -70,12 +76,39 @@ partiti_o_coalizioni = ['SCIACOVELLI SINDACO - CI PIACE!',
        'FORZA ITALIA', 'ROMITO SINDACO', 'PENSIONATI E INVALIDI',
        'OLTRE MANGANO SINDACO', 'COALIZIONE ROMITO', 'COALIZIONE LECCESE', 'COALIZIONE LAFORGIA']
 
-st.title("Elezioni comunali Bari 2024")
-st.write("Esplora la distribuzione dei voti di singoli partiti o coalizioni sul territorio. "
-		 "Ogni esagono corrisponde a un seggio elettorale. I valori sono percentuali sul totale dei voti nel seggio.")
+tab1, tab2 = st.tabs(["Voti sul territorio", "Similarità tra liste"])
 
-party_name = st.selectbox(
-    'Scegli un partito o coalizione',
-     partiti_o_coalizioni)
 
-plot_hexmap_party(filtered_df_voti, party_name)
+with tab1:
+	st.title("Elezioni comunali Bari 2024")
+	st.header("Dove le diverse liste hanno preso più voti?")
+	st.write("Esplora la distribuzione dei voti di singoli partiti o coalizioni sul territorio. "
+			 "Ogni esagono corrisponde a un seggio elettorale. I valori sono percentuali sul totale dei voti nel seggio.")
+
+	party_name = st.selectbox(
+	    'Scegli un partito o coalizione',
+	     partiti_o_coalizioni)
+
+	plot_hexmap_party(df_voti_joined, party_name)
+
+
+with tab2:
+	st.title("Elezioni comunali Bari 2024")
+	st.header("Quanto è simile l'elettorato delle diverse liste?")
+	st.write("Ogni punto nel grafico è un seggio e i valori sugli assi sono le percentuali di voti prese da ciascuna lista in quel seggio. "
+			 "Il grafico rappresenta la correlazione tra le percentuali di voti prese da due liste su tutti i seggi della città. "
+			 "Prova a confrontare, ad esempio, le liste *Laforgia Sindaco* e *Decaro per Bari*.")
+
+	party_1 = st.selectbox(
+	    'Scegli la prima lista',
+	    partiti_o_coalizioni[:-4],
+	    key=1
+	)
+
+	party_2 = st.selectbox(
+	    'Scegli la seconda lista',
+	    partiti_o_coalizioni[:-4],
+	    key=2
+	)
+
+	reg_plot_parties(df_voti_joined, party_1, party_2)
